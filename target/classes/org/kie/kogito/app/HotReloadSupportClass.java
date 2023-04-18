46c079d9a20d0df703093696942b16431b5bf1eb
JPerezAndreu <joaquin_perez@es.ibm.com>
2023-04-18 03:56:43 -0700

    first commit

---
+++ target/classes/org/kie/kogito/app/HotReloadSupportClass.java
@@ -0,0 +1,5 @@
+package org.kie.kogito.app;
+@io.quarkus.runtime.Startup()
+public class HotReloadSupportClass {
+private static final String ID = "1b41edae-6d02-4a90-b838-e03ed847e487";
+}
---
+++ target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
@@ -0,0 +1,5 @@
+package org.kie.kogito.app;
+@io.quarkus.runtime.Startup()
+public class HotReloadSupportClass {
+private static final String ID = "6a818167-f540-4b2c-a4f7-f9c2f042f96c";
+}
